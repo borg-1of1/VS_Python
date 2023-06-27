7483538e74b38e69d17eb986a826c191ec56c895
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2023-06-27 14:26:32 -0500

    edited bitShift to include charter shifting

--- ForMyBoy/bitShift.py
+++ ForMyBoy/bitShift.py
@@ -5,6 +5,36 @@ rightShifted = startingNum>>numberShift
 
 print("The result of shifting {} right by {} bits is: {}.".format(startingNum,numberShift,rightShifted))
 
-leftShifted = startingNum<<numberShift14
+leftShifted = startingNum<<numberShift
 
 print("The result of shifting {} left by {} bits is: {}.".format(startingNum,numberShift,leftShifted))
+
+
+# String and charater shifting
+# Function for rotating left.  Takes in a string and number of characters to shift.  Returns the shifted string.
+def leftShiftString(string, n):
+    charList = list(string)
+    leftRotate = charList[n:]+charList[:n]
+    leftRotatedString = "".join(leftRotate)
+    return leftRotatedString
+
+# Function for rotating right.  Takes in a string and number of characters to shift.  Returns the shifted string.
+def rightShiftString(string, n):
+    charList = list(string)
+    rightRotate = charList[-n:]+charList[:-n]
+    rightRotatedString = "".join(rightRotate)
+    return rightRotatedString
+
+text = (input("Enter a message: "))
+shift = int(input("Enter the distance: "))
+
+leftShift =  leftShiftString(text,shift)
+rightShift = rightShiftString(text,shift)
+
+print("This is what happens when you shift '{}' left by {} places: {}.".format(text,shift,leftShift))
+print("\n")
+print("This is what happens when you shift '{}' right by {} places: {}.".format(text,shift,rightShift))
+
+print(leftShift)
+print("\n")
+print(rightShift)
