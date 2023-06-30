bd0c34fba439867c7d32c7ee5b1f7ef3d67f8f01
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2023-06-29 21:37:29 -0500

    Created a script for copying file contents

---
+++ ForMyBoy/fileCopy.py
@@ -0,0 +1,25 @@
+import shutil
+
+copyFromFile = input("Enter the input file name: ")
+copyToFile = input("Enter the output file name: ")
+
+# Method 1 using shutil - Don't think this is in what you have learned
+# shutil.copyfile(copyFromFile,copyToFile)
+
+# Method 2 - Reading from one file into memory and then writing to the other until all data has been copied
+# Open file
+copyFrom = open(copyFromFile,'r')
+# Read contents to end of file
+copyText = copyFrom.readlines()
+# Close file
+copyFrom.close()
+
+# Open file that will have data added to it
+copyTo = open(copyToFile,'w')
+
+# iterate through each line of text and write to the file
+for lineOfText in copyText:
+    copyTo.write(lineOfText)
+
+# Close the file when done
+copyTo.close()
