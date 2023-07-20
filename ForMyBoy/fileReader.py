f9f38ad2386b73c05e96e5ede326956fc04a297d
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2023-07-20 13:23:35 -0500

    formatted to get 2 decimal places

--- ForMyBoy/fileReader.py
+++ ForMyBoy/fileReader.py
@@ -2,9 +2,11 @@ file1 = input("Enter the input file name: ")
 
 # \t is the way to define a tab
 with open(file1,'r') as f1:
-    print("Name" + "\t" + "Hours" + "\t" + "Total Pay" +"\n")
+    print("Name" + "\t" + "\t" + "Hours" + "\t" + "Total Pay" +"\n")
     for line in f1:
         words = line.split()
-        print(words[0] + "\t" + words[1]+ "\t" + str(float(words[2]) * float(words[1])))
+        pay = float(words[2]) * float(words[1])
+        paid = "{:.2f}".format(pay)
+        print(words[0] + "\t" + "\t" + words[1]+ "\t" + str(paid))
 f1.close()
 
