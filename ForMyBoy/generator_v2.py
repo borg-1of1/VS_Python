848566bd0029e0caa00d0299926b23a56fe11a0c
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2023-08-10 17:16:56 -0500

    moved and updated with new funct

--- ForMyBoy/generator_v2.py
+++ ForMyBoy/generator_v2.py
@@ -16,23 +16,41 @@ verbs = ("HIT", "SAW", "LIKED")
 
 prepositions = ("WITH", "BY")
 
+conjunctions = ("AND", "OR")
+
+adjectives = ("RED", "HARD", "SOFT", "BLUE", "LARGE", "SMALL", "WHITE")
+
 def sentence():
     """Builds and returns a sentence."""
-    return nounPhrase() + " " + verbPhrase()
+    completeSentence = nounPhrase() + " " + verbPhrase() 
+    if(random.random()> 0.5):
+        completeSentence = completeSentence + " " + conjunction() + " " + verbPhrase()
+    return completeSentence
 
 def nounPhrase():
     """Builds and returns a noun phrase."""
     return random.choice(articles) + " " + random.choice(nouns)
 
 def verbPhrase():
-    """Builds and returns a verb phrase."""
-    return random.choice(verbs) + " " + nounPhrase() + " " + \
-           prepositionalPhrase()
+    """Builds and returns a verb phrase."""    
+    if (random.random()> 0.5):
+        phrase = random.choice(verbs) + " " + adjective() + " " + nounPhrase()
+    else:
+        phrase = random.choice(verbs) + " " + nounPhrase()
+    if(random.random()> 0.5):
+        phrase = phrase + " " + prepositionalPhrase()
+    return phrase
+           
 
 def prepositionalPhrase():
     """Builds and returns a prepositional phrase."""
     return random.choice(prepositions) + " " + nounPhrase()
 
+def conjunction():
+    return random.choice(conjunctions)
+
+def adjective():
+    return random.choice(adjectives)
 
 def main():
     """Allows the user to input the number of sentences
