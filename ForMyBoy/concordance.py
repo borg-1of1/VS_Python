c4ae62607b9c35f9fe436077cb5eb682445c4259
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2023-08-11 15:47:36 -0500

    update to concordance.py

--- ForMyBoy/concordance.py
+++ ForMyBoy/concordance.py
@@ -9,14 +9,23 @@ my_file = open(file1,"r")
 # Read data from file
 data = my_file.read()
   
-# create method to coutn the unique words
+# create count list to hold words and their count
 count = {}
 
-for w in data.replace('\n',' ').split(' '):
+# add all of the words to a list
+words = data.replace('\n',' ').split(' ')
+
+# iterate through list to count each word
+for w in words:
     if w in count:
         count[w] += 1
     else:
-        count[w] = 1
-for word, times in count.items():
-    print(word, times)
+        count[w] = 1   
+
+# Sort words into alphabetical order and get rid of duplicates
+sortedWords = sorted(set(count))
+
+# Print words and their count
+for word in sortedWords:     
+    print(word, count[word])
 
