74979bad4df7b77133e7fc68ed6fff673900838e
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2023-08-24 14:33:56 -0500

    Update sort.py

--- ForMyBoy/sort.py
+++ ForMyBoy/sort.py
@@ -1,19 +1,33 @@
+def main():
+    lyst = []
+    print(isSorted(lyst))
+    lyst = [1]
+    print(isSorted(lyst))
+    lyst = list(range(10))
+    print(isSorted(lyst))
+    lyst[9] = 3
+    print(isSorted(lyst))
+
+if __name__ == "__main__":
+    main()
+
+
 def isSorted(lyst):
-if len(lyst) >= 0 and len(lyst) < 2:
-return True
-else:
-for i in range(len(lyst)-1):
-if lyst[i] > lyst [i + 1]:
-return False
-return True
+    if len(lyst) >= 0 and len(lyst) < 2:
+        return True
+    else:
+        for i in range(len(lyst)-1):
+            if lyst[i] > lyst [i + 1]:
+                return False
+                return True
 # A main for testing your code
 def main():
-lyst = []
-print(isSorted(lyst))
-lyst = [1]
-print(isSorted(lyst))
-lyst = list(range(10))
-print(isSorted(lyst))
-lyst[9] = 3
-print(isSorted(lyst))
-main()
+    lyst = []
+    print(isSorted(lyst))
+    lyst = [1]
+    print(isSorted(lyst))
+    lyst = list(range(10))
+    print(isSorted(lyst))
+    lyst[9] = 3
+    print(isSorted(lyst))
+    main()
