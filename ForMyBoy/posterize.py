1d409790e989262b5abcb185526a79ab191e6e65
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2023-09-04 15:38:46 -0500

    updayes based on feedback

--- ForMyBoy/posterize.py
+++ ForMyBoy/posterize.py
@@ -9,22 +9,16 @@ from images import Image
 
 
 """ Write your code here """
-def posterize(imageFile,r, g, b):
-    blackPixel = (0, 0, 0)
-    redPixel = (r,0,0)
-    greenPixel = (0,g,0)
-    bluePixel = (0,0,b)
+def posterize(imageFile,color = (0, 0,0)):    
     whitePixel = (255, 255, 255)
     for y in range(imageFile.getHeight()):
         for x in range(imageFile.getWidth()):
             (r, g, b) = imageFile.getPixel(x, y)
             average = (r + g + b) // 3
-            if average < 85:
-                imageFile.setPixel(x, y, redPixel)
-            elif average < 170:
-                imageFile.setPixel(x, y, greenPixel)
+            if average < 128:
+                imageFile.setPixel(x, y, color)
             else:
-                imageFile.setPixel(x, y, bluePixel)
+                imageFile.setPixel(x, y, whitePixel)
 
 
 def main():
@@ -33,7 +27,7 @@ def main():
     green = int(input("Enter an integer [0..255] for green: "))
     blue = int(input("Enter an integer [0..255] for blue: "))                    
     image = Image(filename)
-    posterize(image, red, green, blue)
+    posterize(image, (red, green, blue))
     image.draw()
 
 if __name__ == "__main__":
