6835fc79b720c398a313daa0d9c6fe4f77a46eb4
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2023-09-05 20:52:40 -0500

    coded button press and fields

--- ForMyBoy/taxformwithgui.py
+++ ForMyBoy/taxformwithgui.py
@@ -16,25 +16,33 @@ class TaxCalculator(EasyFrame):
 
     def __init__(self):
         """Sets up the window and the widgets."""
-        EasyFrame.__init__(self, title = "Tax Calculator")
+        EasyFrame.__init__(self, title = "Tax Calculator",width=300, height=150)
 
-        # Label and field for the income
-        # (self.incomeField)
+        # Label and field for the income        
+        self.addLabel(text="Gross Income",row=0,column=0)
+        self.incomeField = self.addFloatField(value=0.00,row=0,column=1)
 
-        # Label and field for the number of dependents
-        # (self.depField)
+        # Label and field for the number of dependents        
+        self.addLabel(text="Dependents",row=2,column=0)
+        self.depField = self.addIntegerField(value=0,row=2,column=1)
 
         # The command button
+        self.addButton(text="Compute",row=4,column=0,columnspan=2, command=self.computeTax)
 
-        # Label and field for the tax
-        # (self.taxField)
+        # Label and field for the tax        
+        self.addLabel(text="Total Tax",row=6,column=0)
+        self.taxField = self.addFloatField(value=0.00,row=6,column=1)
+
 
     # The event handler method for the button
     def computeTax(self):
         """Obtains the data from the input field and uses
         them to compute the tax, which is sent to the
         output field."""
-        pass
+        income = self.incomeField.getNumber()
+        numDependents = self.depField.getNumber()        
+        tax = (income - numDependents) * .072
+        self.taxField.setNumber(tax)
         
         
 def main():
