544cbc7688cbd515b121985e7091f9f44781231f
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2023-10-05 20:24:09 -0500

    coded doctor class

--- ForMyBoy/doctor.py
+++ ForMyBoy/doctor.py
@@ -10,55 +10,66 @@ import random
 
 history = []
 
- # All doctors share the same qualifiers, replacements, and hedges.
+ # All doctors share the same qualifiers, replacements, and hedges.    
 
-    QUALIFIERS = ['Why do you say that ', 'You seem to think that ',
+QUALIFIERS = ['Why do you say that ', 'You seem to think that ',
                   'Did I just hear you say that ',
                   'Why do you believe that ']
 
-    REPLACEMENTS = {'i': 'you', 'me': 'you', 'my': 'your',
+REPLACEMENTS = {'i': 'you', 'me': 'you', 'my': 'your',
                     'we': 'you', 'us': 'you', 'am': 'are',
                     'you': 'i', 'you': 'I'}
 
-    HEDGES = ['Go on.', 'I would like to hear more about that.',
-              'And what do you think about this?', 'Please continue.'] 
+HEDGES = ['Go on.', 'I would like to hear more about that.',
+              'And what do you think about this?', 'Please continue.']
+class Doctor(object):
+     
 
-def reply(sentence):
-    """Implements three different reply strategies."""
-    probability = random.randint(1, 5)
-    if probability in (1, 2):
-        # Just hedge
-        answer = random.choice(hedges)
-    elif probability == 3 and len(history) > 3:
-        # Go back to an earlier topic
-        answer = "Earlier you said that " + \
+    def greeting():
+        greeting = "Hello, how can I help you?"
+        return greeting
+
+    def reply(sentence):
+        """Implements three different reply strategies."""
+        probability = random.randint(1, 5)
+        if probability in (1, 2):
+            # Just hedge
+            answer = random.choice(HEDGES)
+        elif probability == 3 and len(history) > 3:
+            # Go back to an earlier topic
+            answer = "Earlier you said that " + \
                  changePerson(random.choice(history))
-    else:
-        # Transform the current input
-        answer = random.choice(qualifiers) + changePerson(sentence)
-    # Always add the current sentence to the history list
-    history.append(sentence)
-    return answer
+        else:
+            # Transform the current input
+            answer = random.choice(QUALIFIERS) + Doctor.changePerson(sentence)
+        # Always add the current sentence to the history list
+        history.append(sentence)
+        return answer
 
-def changePerson(sentence):
-    """Replaces first person pronouns with second person
-    pronouns."""
-    words = sentence.split()
-    replyWords = []
-    for word in words:
-        replyWords.append(replacements.get(word, word))
-    return " ".join(replyWords) 
+    def changePerson(sentence):
+        """Replaces first person pronouns with second person
+        pronouns."""
+        words = sentence.split()
+        replyWords = []
+        for word in words:
+           replyWords.append(REPLACEMENTS.get(word, word))
+        return " ".join(replyWords) 
+
+    def fairwell():
+        so_long = "Have a nice day!"
+        return so_long
 
 def main():
     """Handles the interaction between patient and doctor."""
-    print("Good morning, I hope you are well today.")
-    print("What can I do for you?")
+    #print("Good morning, I hope you are well today.")
+    #print("What can I do for you?")
+    print(Doctor.greeting())
     while True:
         sentence = input("\n>> ")
         if sentence.upper() == "QUIT":
-            print("Have a nice day!")
+            print(Doctor.fairwell())
             break
-        print(reply(sentence))
+        print(Doctor.reply(sentence))
 
 # The entry point for program execution
 if __name__ == "__main__":
